2cf1a470857c1681a486e5ba82c0b30dab1269b4
Carlos Lancha <carlos.lancha@liferay.com>
2020-09-30 22:49:19 +0200

    Create drawing app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
+import CanvasDraw from "react-canvas-draw";
+import {ClayCheckbox} from '@clayui/form';
+import ClayColorPicker from '@clayui/color-picker';
+import ClayButton from "@clayui/button";
+import ClayLayout from "@clayui/layout";
+import ClayList from '@clayui/list';
+
+// Imports the @clayui/css package CSS
+import "@clayui/css/lib/css/atlas.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const canvasDrawRef = useRef();
+
+	const [bgColor, setBgColor] = useState('#FFFFFF');
+	const [bgTransparent, setBgTransparent] = useState(false);
+	const [brushColor, setBrushColor] = useState('#000000');
+
+	return (
+		<ClayLayout.ContainerFluid view>
+			<ClayLayout.Row className="text-center">
+				<ClayLayout.Col size={2} >
+					<h2>Tools</h2>
+
+					<ClayList>
+						<ClayList.Item flex>
+							<ClayList.ItemField	className="ml-1">
+								<ClayColorPicker
+									onValueChange={(value) => {
+										setBrushColor(`#${value}`);
+									}}
+									showHex={false}
+									title="Brush Color"
+									value={brushColor}
+								/>
+							</ClayList.ItemField>
+						</ClayList.Item>
+
+						<ClayList.Item flex>
+							<ClayList.ItemField	className="ml-1">
+								<ClayColorPicker
+									onValueChange={(value) => {
+										setBgColor(`#${value}`);
+									}}
+									showHex={false}
+									title="Background Color"
+									value={bgColor}
+								/>
+
+								<br/>
+
+								<ClayCheckbox
+									checked={bgTransparent}
+									label="Transparent"
+									onChange={() => setBgTransparent(bgTransparent => !bgTransparent)}
+								/>
+							</ClayList.ItemField>
+						</ClayList.Item>
+					</ClayList>
+
+					<ClayButton
+						className="mr-2"
+						displayType="primary"
+						onClick={() => canvasDrawRef.current.clear()}
+					>
+						Clear
+					</ClayButton>
+
+					<ClayButton
+						className="ml-2"
+						displayType="primary"
+					>
+						Save
+					</ClayButton>
+				</ClayLayout.Col>
+
+				<ClayLayout.Col size={10}>
+					<CanvasDraw
+						ref={canvasDrawRef}
+						style={{
+							backgroundColor: bgTransparent ? 'transparent' : bgColor
+						}}
+						//loadTimeOffset: 5,
+						lazyRadius={0}
+						brushRadius={2}
+						brushColor={brushColor}
+						//catenaryColor: "#0a0302",
+						//gridColor: "rgba(150,150,150,0.17)",
+						//hideGrid={true}
+						canvasWidth="100%"
+						//canvasHeight="100%"
+						//disabled: false,
+						//imgSrc: "",
+						//saveData={drawData}
+						//immediateLoading: false,
+						//hideInterface={true}
+					/>
+				</ClayLayout.Col>
+			</ClayLayout.Row>
+		</ClayLayout.ContainerFluid>
+	);
 }
 
 export default App;
